4dec05c59a7bb56872b452c792a1d5bbb8fd1f5f
clarababette <clara_coetzee@zoho.com>
2021-03-16 17:35:14 +0200

    calculate-bill and text-bill completed

--- js/text-bill.js
+++ js/text-bill.js
@@ -10,3 +10,47 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+
+var billTypeText = document.querySelector(".billTypeText");
+var addToBillBtn = document.querySelector(".addToBillBtn");
+var callsTotalElem = document.querySelector(".callTotalOne");
+var smsTotalElem = document.querySelector(".smsTotalOne");
+var totalCostElem = document.querySelector(".totalOne");
+var callsTotal = 0;
+var smsTotal = 0;
+
+function textBillTotal() {
+    var billTextEntered = billTypeText.value.trim();
+
+    switch (billTextEntered) {
+        case "call":
+            callsTotal += 2.75;
+            break;
+        case "sms":
+            smsTotal += 0.75;
+            break;
+        case "clear":
+            callsTotal = 0;
+            smsTotal = 0;
+            totalCostElem.classList.remove("danger");
+            totalCostElem.classList.remove("warning");
+            break;
+        default:
+            billTypeText.value = "Try again!"
+            break;
+    }
+
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
+
+    if (totalCost > 50) {
+        totalCostElem.classList.add("danger");
+    }
+    else if (totalCost > 30){
+        totalCostElem.classList.add("warning");
+    }
+};
+
+addToBillBtn.addEventListener("click", textBillTotal);
