4a0e19facd1072ecf08d45bbb73eb68b3aa1ae89
clarababette <clara_coetzee@zoho.com>
2021-04-21 10:29:23 +0200

    handlebars demo 1

--- js/text-bill-dom.js
+++ js/text-bill-dom.js
@@ -1,34 +1,80 @@
+var thisBill = {
+        widgName : 'Text input bill',
+        instructions : ["Enter call or sms in the textbox. Update the appropriate total and the global total when the ADD button in pressed.","Calls costs R2.75 and a SMS R0.75", "If the total cost exceed R30.00 show the total cost in orange, if over R50 show it in red."],
+        text : true,
+        addBtnName : "textBillAdd",
+        resetBtnName : "textBillReset",
+        callCount : 0,
+        smsCount : 0,
+        callTotal : '0.00',
+        smsTotal : '0.00',
+        sumTotal : '0.00',
+        level : ''
 
-
-var billTypeText = document.querySelector(".billTypeText");
-var addToBillBtn = document.querySelector(".addToBillBtn");
-var callsTextTotalElem = document.querySelector(".callTotalOne");
-var smsTextTotalElem = document.querySelector(".smsTotalOne");
-var totalTextCostElem = document.querySelector(".totalOne");
-var textBillResetBtn = document.querySelector(".textBillResetBtn");
-
-var theTextBill = billTextRadio();
-
-function displayValues() {
-    callsTextTotalElem.innerHTML = theTextBill.getCallTotal();
-    smsTextTotalElem.innerHTML = theTextBill.getSMSTotal();
-    totalTextCostElem.innerHTML = theTextBill.getTotalCost();
-    document.querySelector(".callCountTwo").innerHTML = theTextBill.getCallCount();
-    document.querySelector(".smsCountTwo").innerHTML = theTextBill.getSMSCount();
-}
-
-addToBillBtn.addEventListener("click", function() {
-    theTextBill.addItem(billTypeText.value);
-    displayValues();
-    totalTextCostElem.classList.add(theTextBill.getClass());
+    }
+    var theTextBill = billTextRadio();
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        var templateSource = document.querySelector(".billsTemplate").innerHTML;
+        var billsTemplate = Handlebars.compile(templateSource);
+        var textBill = document.querySelector(".textBill");
+        
+        // var thisBill = billsTemplate({
+            //     widgName : 'Text input bill',
+            //     instructions : ["Enter call or sms in the textbox. Update the appropriate total and the global total when the ADD button in pressed.","Calls costs R2.75 and a SMS R0.75", "If the total cost exceed R30.00 show the total cost in orange, if over R50 show it in red."],
+            //     text : true,
+            //     addBtnName : "textBillAdd",
+            //     resetBtnName : "textBillReset",
+            //     callCount : 0,
+            //     smsCount : 0,
+            //     callTotal : '0.00',
+            //     smsTotal : '0.00',
+            //     sumTotal : '0.00',
+            //     level : ''
+            
+            // });
+            console.log(billsTemplate(thisBill));
+            console.log(textBill);
+            textBill.innerHTML = billsTemplate(thisBill);
+            
+            
+            
+            document.querySelector('.textBillAdd').addEventListener("click", function() {
+        var billTypeText = document.querySelector(".billTypeText");
+        alert("clicked")
+        theTextBill.addItem(billTypeText.value);
+        displayValues();
+        level = theTextBill.getClass();
+        textBill.innerHTML = billsTemplate(thisBill);
+    });
+    
+    function displayValues() {
+        thisBill.callTotal = theTextBill.getCallTotal();
+        thisBill.smsTotal = theTextBill.getSMSTotal();
+        thisBill.smsTotal = theTextBill.getTotalCost();
+        thisBill.callCount = theTextBill.getCallCount();
+        thisBill.smsCount = theTextBill.getSMSCount();
+    }
 });
 
-textBillResetBtn.addEventListener("click", function() {
-    theTextBill = billTextRadio();
-    totalTextCostElem.classList.remove("danger");
-    totalTextCostElem.classList.remove("warning");
-    displayValues();
-})
+
+
+// var addToBillBtn = document.querySelector(".addToBillBtn");
+// var callsTextTotalElem = document.querySelector(".callTotalOne");
+// var smsTextTotalElem = document.querySelector(".smsTotalOne");
+// var totalTextCostElem = document.querySelector(".totalOne");
+// var textBillResetBtn = document.querySelector(".textBillResetBtn");
+
+//
+
+
+
+// textBillResetBtn.addEventListener("click", function() {
+//     theTextBill = billTextRadio();
+//     totalTextCostElem.classList.remove("danger");
+//     totalTextCostElem.classList.remove("warning");
+//     displayValues();
+// })
 
 
 
